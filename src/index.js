790a7dc0d6c7d36e40d95a86e313be8cd1e762be
Ryan Chua <ryan.s.chua@gmail.com>
2019-05-30 22:34:22 -0700

    add cors; restructure return data

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
 const port = 4000;
 
+app.use(cors());
 app.use('/api', router);
 
 router.get('/users', (req, res) => {
-  res.send({
-    data: [
+  res.json(
+    [
       {
         id: 1,
         name: 'Griffin McElroy',
         email: 'griffin.mcelroy@maximumfun.org',
         birthday: 'April 17, 1987',
-        zipCode: 25755
+        zipcode: 25755
       },
       {
         id: 2,
         name: 'Travis McElroy',
         email: 'travis.mcelroy@maximumfun.org',
         birthday: 'November 8, 1983',
-        zipCode: 25755
+        zipcode: 25755
       },
       {
         id: 2,
         name: 'Justin McElroy',
         email: 'justin.mcelroy@maximumfun.org',
         birthday: 'November 8, 1980',
-        zipCode: 25755
+        zipcode: 25755
       },
     ]
-  })
+  )
 })
 
 app.listen(port);
