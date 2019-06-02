41e2a9460bcdd73cd58a6a20f60d075e2776540a
Ryan Chua <ryan.s.chua@gmail.com>
2019-06-01 17:52:25 -0700

    add routes for post and delete

---
+++ mocks/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Griffin McElroy',
+    email: 'griffin.mcelroy@maximumfun.org',
+    birthday: 'April 17, 1987',
+    zipcode: 25755
+  },
+  {
+    id: 2,
+    name: 'Travis McElroy',
+    email: 'travis.mcelroy@maximumfun.org',
+    birthday: 'November 8, 1983',
+    zipcode: 25755
+  },
+  {
+    id: 3,
+    name: 'Justin McElroy',
+    email: 'justin.mcelroy@maximumfun.org',
+    birthday: 'November 8, 1980',
+    zipcode: 25755
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 const app = express();
 const router = express.Router();
 const port = 4000;
 
+let users = require('../mocks/users')
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api', router);
 
 router.get('/users', (req, res) => {
-  res.json(
-    [
-      {
-        id: 1,
-        name: 'Griffin McElroy',
-        email: 'griffin.mcelroy@maximumfun.org',
-        birthday: 'April 17, 1987',
-        zipcode: 25755
-      },
-      {
-        id: 2,
-        name: 'Travis McElroy',
-        email: 'travis.mcelroy@maximumfun.org',
-        birthday: 'November 8, 1983',
-        zipcode: 25755
-      },
-      {
-        id: 3,
-        name: 'Justin McElroy',
-        email: 'justin.mcelroy@maximumfun.org',
-        birthday: 'November 8, 1980',
-        zipcode: 25755
-      },
-    ]
-  )
+  res.json(users)
+})
+
+router.post('/users', jsonParser, (req, res) => {
+  users.push(req.body)
+  res.json(users)
+})
+
+router.delete('/users/:id', (req, res) => {
+  users = users.filter(user => user.id !== parseInt(req.params.id, 10))
+  res.json(users)
 })
 
 app.listen(port);
